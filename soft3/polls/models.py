fcf801062465642884d693086b24eede8e6aa26d
Susana Medina <smedinag@usbcali.edu.co>
2022-05-23 17:50:50 -0500

    6to commit con unity tests

--- soft3/polls/models.py
+++ soft3/polls/models.py
@@ -1,6 +1,7 @@
+import datetime
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,7 +11,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -19,3 +22,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
